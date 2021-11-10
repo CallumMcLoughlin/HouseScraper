e6e1c139f9fd5c34b6bb8fe6244eef19fbabdccc
CallumMcLoughlin <59436970+CallumMcLoughlin@users.noreply.github.com>
2021-11-10 21:00:30 +1300

    Update README, document program.cs

--- Program.cs
+++ Program.cs
@@ -5,11 +5,25 @@ using HouseScraper.Scraper.Web;
 
 namespace HouseScraper
 {
+    /// <summary>
+    /// Main entrypoint
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Bot used to send notifications
+        /// </summary>
         private static readonly NotificationBot Bot = new NotificationBot();
+        
+        /// <summary>
+        /// Property checker that checks for new properties uploaded
+        /// </summary>
         private static readonly PropertyChecker Checker = new PropertyChecker(new HtmlPropertyScraper());
         
+        /// <summary>
+        /// Main entrypoint
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Bot.BotReady += (sender, client) =>
@@ -20,6 +34,11 @@ namespace HouseScraper
             Bot.RunAsync().GetAwaiter().GetResult();
         }
 
+        /// <summary>
+        /// Event for when a new property has been found
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
         private static void OnNewProperty(object sender, PropertyFoundEventArgs eventArgs)
         {
             Console.WriteLine(eventArgs.Property.PropertyTile);
